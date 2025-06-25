41dacefbbc28d7b2a0ac6234bbb963e8811f3b66
Eder Raul Perez Gallardo Hernandez <ederaul_@hotmail.com>
2025-06-25 17:48:08 -0600

    Add files via upload

---
+++ sqlcreaTablatriaje.sql
@@ -0,0 +1,29 @@
+use triaje;
+
+--drop table Triage;
+
+/*crea la tabla en sql server*/
+CREATE TABLE Triage (
+    id INT IDENTITY PRIMARY KEY,
+    nomina VARCHAR(50),
+    nombre VARCHAR(150),
+    sexo VARCHAR(20),
+    edad INT,
+    medico VARCHAR(150),
+    fecha_hora DATETIME,
+    pregunta_1 INT,
+    pregunta_2 INT,
+    pregunta_3 INT,
+    pregunta_4 INT,
+    pregunta_5 INT,
+    pregunta_6 INT,
+	pregunta_7 INT,
+	pregunta_8 INT,
+	pregunta_9 INT,
+	pregunta_10 INT,
+	pregunta_11 INT
+
+
+	)
+/*tabla creada*/
+	select *from Triage
